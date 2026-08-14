5a0987d525ff9cfd282b81832a83fde1eecc40e8
Saima-Waikar <saimawaikar2021@gmail.com>
2026-08-14 17:54:19 +0530

    Add files via upload

---
+++ Test_1/T2.py
@@ -0,0 +1,7 @@
+p = int(input('Enter Principle:'))
+r = float(input('Enter Rate:'))
+t = int(input('Enter Time(Year):'))
+
+simple_interest = (p*r*t)/100
+
+print(f'Simple Interest is {simple_interest}')
