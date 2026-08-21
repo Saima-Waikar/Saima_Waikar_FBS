e3c280c56019d3e9e98c3f60a1399772bed965dc
Saima-Waikar <saimawaikar2021@gmail.com>
2026-08-21 19:17:08 +0530

    Delete Demos/Basics/Patterns directory

--- Demos/Basics/Patterns/p8.py
+++
@@ -1,7 +0,0 @@
-for i in range(1, 6):          # rows
-    for j in range(1, 6):      # columns
-        if (i + j) % 2 == 0:
-            print("*", end=" ")
-        else:
-            print("$", end=" ")
-    print()
